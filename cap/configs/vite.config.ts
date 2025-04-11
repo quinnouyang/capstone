763371b51cf9f709fafb9f09a72c4374e29e60a5
quinnouyang <90884224+quinnouyang@users.noreply.github.com>
2025-04-11 15:52:14 -0500

    [dev] Add wdyr

--- cap/configs/vite.config.ts
+++ cap/configs/vite.config.ts
@@ -1,6 +1,14 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
+// https://github.com/welldone-software/why-did-you-render/issues/243#issuecomment-1132892461
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      jsxImportSource:
+        process.env.NODE_ENV === "development"
+          ? "@welldone-software/why-did-you-render"
+          : "react",
+    }),
+  ],
 });
--- cap/src/main.tsx
+++ cap/src/main.tsx
@@ -1,3 +1,5 @@
+import "./wdyr";
+
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
