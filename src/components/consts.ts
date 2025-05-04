cc978020392feac7f9647b57f3a015f3c7192e97
quinnouyang <90884224+quinnouyang@users.noreply.github.com>
2025-05-03 19:28:44 -0500

    [devtools] Patch dumb Zustand devtool bug with BigInt and it'll probably hurt me more soon

--- src/components/consts.ts
+++ src/components/consts.ts
@@ -10,9 +10,13 @@ import src1 from "../../assets/2.wav";
 import src2 from "../../assets/3.wav";
 
 export const INIT_NODES: AudioTrackNode[] = [
-  initNode(0, { x: 0, y: 0 }, { src: src0 }),
-  initNode(1, { x: 800, y: 200 }, { src: src2 }),
-  initNode(2, { x: 800, y: -200 }, { src: src1 }),
+  initNode(0, { x: 0, y: 0 }, { src: src0, duration: -3, currentTime: -2 }),
+  initNode(1, { x: 800, y: 200 }, { src: src2, duration: -3, currentTime: -2 }),
+  initNode(
+    2,
+    { x: 800, y: -200 },
+    { src: src1, duration: -3, currentTime: -2 },
+  ),
 ];
 
 export const INIT_EDGES: Edge[] = addEdge(
