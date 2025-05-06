8039a4d7970d1b5f5dc67f17e7ad26bcd48dafb7
quinnouyang <90884224+quinnouyang@users.noreply.github.com>
2025-05-06 01:17:37 -0500

    [audio] Init ctx as suspended

--- src/store.ts
+++ src/store.ts
@@ -60,116 +60,122 @@ export type State = {
  */
 const useCustomStore = createWithEqualityFn<State>()(
   // devtools(
-  (set, get) => ({
-    /**
-     * ReactFlow
-     */
-    nodes: INIT_NODES,
-    edges: INIT_EDGES,
-    nodeCount: INIT_NODES.length,
-    edgeCount: INIT_EDGES.length,
+  (set, get) => {
+    const ctx = new AudioContext();
+    ctx.suspend();
 
-    getNode: (id) => {
-      const node = get().nodes.find((n) => n.id === id);
-      if (!node) throw Error(`Could not find node of ID ${id}`);
-      return node;
-    },
-    getEdge: (id) => {
-      const edge = get().edges.find((e) => e.id === id);
-      if (!edge) throw Error(`Could not find edge of ID ${id}`);
-      return edge;
-    },
-    addNodes: (nodes) => {
-      set({ nodes: get().nodes.concat(nodes) });
-      get().refreshCounts();
-    },
-    addEdges: (edges) => {
-      set({ edges: get().edges.concat(edges) });
-      get().refreshCounts();
-    },
-    addEdge: (edge) => {
-      set({ edges: addEdge(edge, get().edges) });
-      get().refreshCounts();
-    },
+    return {
+      /**
+       * ReactFlow
+       */
+      nodes: INIT_NODES,
+      edges: INIT_EDGES,
+      nodeCount: INIT_NODES.length,
+      edgeCount: INIT_EDGES.length,
 
-    onNodesChange: (changes) => {
-      set({
-        nodes: applyNodeChanges(changes, get().nodes),
-      });
-      get().refreshCounts();
-    },
-    onEdgesChange: (changes) => {
-      set({
-        edges: applyEdgeChanges(changes, get().edges),
-      });
-      get().refreshCounts();
-    },
-    onConnect: (connection) => {
-      get().addEdge(connection);
-      get().refreshCounts();
-    },
+      getNode: (id) => {
+        const node = get().nodes.find((n) => n.id === id);
+        if (!node) throw Error(`Could not find node of ID ${id}`);
+        return node;
+      },
+      getEdge: (id) => {
+        const edge = get().edges.find((e) => e.id === id);
+        if (!edge) throw Error(`Could not find edge of ID ${id}`);
+        return edge;
+      },
+      addNodes: (nodes) => {
+        set({ nodes: get().nodes.concat(nodes) });
+        get().refreshCounts();
+      },
+      addEdges: (edges) => {
+        set({ edges: get().edges.concat(edges) });
+        get().refreshCounts();
+      },
+      addEdge: (edge) => {
+        set({ edges: addEdge(edge, get().edges) });
+        get().refreshCounts();
+      },
 
-    updateNodeData(id, data) {
-      set({
-        nodes: get().nodes.map((node) =>
-          node.id === id ? { ...node, data: { ...node.data, ...data } } : node,
-        ),
-      });
-    },
-    refreshCounts: () => {
-      set({
-        nodeCount: get().nodes.length,
-        edgeCount: get().edges.length,
-      });
-    },
-    getOutputNodes: (id) => {
-      return get()
-        .edges.filter((e) => e.source === id)
-        .map((e) => get().getNode(e.target));
-    },
+      onNodesChange: (changes) => {
+        set({
+          nodes: applyNodeChanges(changes, get().nodes),
+        });
+        get().refreshCounts();
+      },
+      onEdgesChange: (changes) => {
+        set({
+          edges: applyEdgeChanges(changes, get().edges),
+        });
+        get().refreshCounts();
+      },
+      onConnect: (connection) => {
+        get().addEdge(connection);
+        get().refreshCounts();
+      },
 
-    /**
-     * Audio
-     */
-    ctx: new AudioContext(), // [Decide] Init as suspended?
+      updateNodeData(id, data) {
+        set({
+          nodes: get().nodes.map((node) =>
+            node.id === id
+              ? { ...node, data: { ...node.data, ...data } }
+              : node,
+          ),
+        });
+      },
+      refreshCounts: () => {
+        set({
+          nodeCount: get().nodes.length,
+          edgeCount: get().edges.length,
+        });
+      },
+      getOutputNodes: (id) => {
+        return get()
+          .edges.filter((e) => e.source === id)
+          .map((e) => get().getNode(e.target));
+      },
 
-    isPlaying: false,
-    nodeIdToEl: new Map<string, HTMLAudioElement>(),
-    currTime: 0,
-    totalTime: 0,
+      /**
+       * Audio
+       */
+      ctx,
+      isPlaying: false,
+      nodeIdToEl: new Map<string, HTMLAudioElement>(),
+      currTime: 0,
+      totalTime: 0,
 
-    togglePlay: () => {
-      set({ isPlaying: !get().isPlaying });
-      const ctx = get().ctx;
-      ctx.state === "suspended" ? ctx.resume() : ctx.suspend();
-      const firstId = get().nodes.at(0)?.id;
-      if (firstId) get().play(firstId);
-    },
-    createAudioNodeSource: (el) => {
-      if (get().nodeIdToEl.has(el.id))
-        return console.warn("Audio source already exists", el.id);
+      togglePlay: () => {
+        set({ isPlaying: !get().isPlaying });
+        const ctx = get().ctx;
+        ctx.state === "suspended" ? ctx.resume() : ctx.suspend();
+        const firstId = get().nodes.at(0)?.id;
+        if (firstId) get().play(firstId);
+      },
+      createAudioNodeSource: (el) => {
+        if (get().nodeIdToEl.has(el.id))
+          return console.warn("Audio source already exists", el.id);
 
-      const srcNode = get().ctx.createMediaElementSource(el);
-      srcNode.connect(get().ctx.destination);
+        const srcNode = get().ctx.createMediaElementSource(el);
+        srcNode.connect(get().ctx.destination);
 
-      // https://zustand.docs.pmnd.rs/guides/maps-and-sets-usage (Necessary?)
-      set(({ nodeIdToEl }) => ({
-        nodeIdToEl: new Map(nodeIdToEl).set(el.id, el),
-      }));
-    },
-    play: (id) => {
-      const srcNode = get().nodeIdToEl.get(id);
-      if (!srcNode) return console.warn("Could not find audio source", id);
-      srcNode.currentTime = 0;
-      srcNode.play();
-    },
-    setCurrTime: (time) => {
-      set({ currTime: time });
-    },
-    setTotalTime: (time) => {
-      set({ totalTime: time });
-    },
-  }),
+        // https://zustand.docs.pmnd.rs/guides/maps-and-sets-usage (Necessary?)
+        set(({ nodeIdToEl }) => ({
+          nodeIdToEl: new Map(nodeIdToEl).set(el.id, el),
+        }));
+      },
+      play: (id) => {
+        const srcNode = get().nodeIdToEl.get(id);
+        if (!srcNode) return console.warn("Could not find audio source", id);
+        srcNode.currentTime = 0;
+        srcNode.play();
+      },
+      setCurrTime: (time) => {
+        set({ currTime: time });
+      },
+      setTotalTime: (time) => {
+        set({ totalTime: time });
+      },
+    };
+  },
   //   {
   //     // [PATCH] Redux DevTools can't serialize BigInt: https://github.com/reduxjs/redux-devtools/issues/1541
   //     serialize: {
