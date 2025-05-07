589bee42b63e46badc1148ca0df803e4b7f5691d
quinnouyang <90884224+quinnouyang@users.noreply.github.com>
2025-05-07 12:59:03 -0500

    [nodes] Add arrows to edges and increase handle sizes

--- src/components/consts.ts
+++ src/components/consts.ts
@@ -1,4 +1,4 @@
-import { addEdge, type Edge } from "@xyflow/react";
+import { MarkerType, type Edge } from "@xyflow/react";
 
 import src0 from "../../assets/1.wav";
 import src2 from "../../assets/3.wav";
@@ -12,47 +12,28 @@ export const INIT_NODES: AudioClipNodeType[] = [
   initNode(2, { x: 800, y: -200 }, src1),
 ];
 
-export const INIT_EDGES: Edge[] = addEdge(
-  {
-    source: INIT_NODES[0].id,
-    target: INIT_NODES[1].id,
+export function initEdge(source: string, target: string): Edge {
+  return {
+    id: `${source}-${target}`,
+    source,
+    target,
     sourceHandle: "out",
     targetHandle: "in",
-  },
-  addEdge(
-    {
-      source: INIT_NODES[0].id,
-      target: INIT_NODES[2].id,
-      sourceHandle: "out",
-      targetHandle: "in",
+    markerEnd: {
+      type: MarkerType.ArrowClosed,
+      width: 36,
+      height: 36,
     },
-    addEdge(
-      {
-        source: INIT_NODES[1].id,
-        target: INIT_NODES[0].id,
-        sourceHandle: "out",
-        targetHandle: "in",
-      },
-      addEdge(
-        {
-          source: INIT_NODES[2].id,
-          target: INIT_NODES[0].id,
-          sourceHandle: "out",
-          targetHandle: "in",
-        },
-        addEdge(
-          {
-            source: INIT_NODES[1].id,
-            target: INIT_NODES[2].id,
-            sourceHandle: "out",
-            targetHandle: "in",
-          },
-          [],
-        ),
-      ),
-    ),
-  ),
-);
+  };
+}
+
+export const INIT_EDGES: Edge[] = [
+  initEdge(INIT_NODES[0].id, INIT_NODES[1].id),
+  initEdge(INIT_NODES[0].id, INIT_NODES[2].id),
+  initEdge(INIT_NODES[1].id, INIT_NODES[0].id),
+  initEdge(INIT_NODES[2].id, INIT_NODES[0].id),
+  initEdge(INIT_NODES[1].id, INIT_NODES[2].id),
+];
 
 export const NODE_TYPES = {
   audioTrackNode: AudioClipNode,
